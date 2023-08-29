4cd567efcc52030a3090d5fa54e76176c2c69088
phat-always-sleeping <tkphat21@apcs.fitus.edu.vn>
2023-08-29 23:48:38 +0700

    build(common): set up firebase authentication

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:flow_builder/flow_builder.dart';
 import 'package:task_managing_application/assets/assets.dart';
 import 'package:task_managing_application/repositories/repositories.dart';
 import 'package:task_managing_application/states/states.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 

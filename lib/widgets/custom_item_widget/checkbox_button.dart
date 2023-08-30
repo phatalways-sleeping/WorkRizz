cc781611005c7e3b0c5910baef0f76fb82ae156d
ntnblune <michealbradcute@gmail.com>
2023-08-30 18:37:16 +0700

    fix checkbox

--- lib/widgets/custom_item_widget/checkbox_button.dart
+++ lib/widgets/custom_item_widget/checkbox_button.dart
@@ -13,35 +13,16 @@ class _CheckboxWidgetState extends State<CheckboxWidget> {
 
   @override
   Widget build(BuildContext context) {
-    Color getColor(Set<MaterialState> states) {
-      const Set<MaterialState> interactiveStates = <MaterialState>{
-        MaterialState.pressed,
-        MaterialState.hovered,
-        MaterialState.focused,
-      };
-      if (states.any(interactiveStates.contains)) {
-        return GREEN;
-      }
-      // if it is ticked change to black
-      if (isChecked) {
-        return BLACK;
-      }
-      return WHITE;
-    }
-
-    BorderSide getBorder(Set<MaterialState> states) {
-      return const BorderSide(color: BLACK, width: BORDER_WIDTH);
-    }
 
     return Checkbox(
       checkColor: WHITE,
       // change show border color always
-      side: MaterialStateBorderSide.resolveWith(getBorder),
+      side: MaterialStateBorderSide.resolveWith((states) => const BorderSide(color: BLACK, width: BORDER_WIDTH)),
       // round corner 
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(SMALL_CORNER)),
       ),
-      fillColor: MaterialStateProperty.resolveWith(getColor),
+      fillColor: MaterialStateProperty.resolveWith((states) => isChecked ? BLACK : WHITE),
       value: isChecked,
       onChanged: (bool? value) {
         setState(() {

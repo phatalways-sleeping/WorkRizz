40bbcae9e452eb967eb64499f1c196ad81a91da2
ntnblune <michealbradcute@gmail.com>
2023-08-30 16:06:13 +0700

    fix colors

--- lib/assets/themes/light_theme.dart
+++ lib/assets/themes/light_theme.dart
@@ -3,10 +3,12 @@ import 'package:task_managing_application/assets/fonts/text_theme_builder.dart';
 
 mixin LightTheme {
   static const Color primaryColor = Color(0xFF5CD669); // 5CD669
-  static const Color accentColor = Color(0xFF9C9AFF); // 9C9AFF
-  static const Color backgroundColor = Color(0xFFDDDDDD); // DDDDDD
+  static const Color secondaryColor = Color(0xFF9C9AFF); // 9C9AFF
+  static const Color tertiaryColor = Color(0xFFEAB0FC); // 9C9AFF
+  static const Color shadowColor = Color(0xFFDDDDDD); // 9C9AFF
+  static const Color backgroundColor = Colors.white; // DDDDDD
   static const Color buttonColor = Color(0xFFF6BB54); // F6BB54
-  static const Color errorColor = Colors.red;
+  static const Color errorColor = Color(0xFFFFAFAF);
 
   static final ThemeData theme = ThemeData(
     textTheme: TextThemeBuilder.robotoTextTheme,
@@ -16,15 +18,17 @@ mixin LightTheme {
     ),
     colorScheme: const ColorScheme(
       primary: primaryColor,
-      secondary: accentColor,
+      secondary: secondaryColor,
+      tertiary: tertiaryColor,
       surface: Colors.white,
       background: backgroundColor,
       error: errorColor,
       onPrimary: Colors.white,
-      onSecondary: Colors.white,
+      onSecondary: Colors.black,
+      onErrorContainer: Colors.black,
       onSurface: Colors.black,
-      onBackground: Colors.black,
-      onError: Colors.white,
+      onBackground: Colors.white,
+      onError: Colors.black,
       brightness: Brightness.light,
     ),
   );

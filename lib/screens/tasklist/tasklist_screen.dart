f4f813ea4fd7c0e7ca035b79b91028970ac876fc
ntnblune <michealbradcute@gmail.com>
2023-09-02 12:36:35 +0700

    fix conflict

--- lib/screens/tasklist/tasklist_screen.dart
+++ lib/screens/tasklist/tasklist_screen.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:task_managing_application/assets/models/task/task_data.dart';
+import 'package:task_managing_application/widgets/custom_hea_bar/custom_header_bar.dart';
 import 'package:task_managing_application/widgets/tasklist/tasklist_view.dart';
+import 'package:task_managing_application/widgets/widgets.dart';
 
 class TaskListScreen extends StatelessWidget {
   const TaskListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: TaskListView(ava: Icon(Icons.person), task: instance1)
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+              const SliverPersistentHeader(
+                pinned: true,
+                delegate: CustomHeaderBar(
+                  username: 'Liana',
+                  imageUrl:
+                      'https://flutter.github.io/assets-for-api-docs/assets/widgets/falcon.jpg',
+                ),
+              ),
+            SliverToBoxAdapter(
+              child: TaskListView(ava: Icon(Icons.person), task: instance1),
+            ),
+          ],
+        ),
+      ),
+      extendBody: true,
+      bottomNavigationBar: CustomNavigationBar(),
     );
   }
 }

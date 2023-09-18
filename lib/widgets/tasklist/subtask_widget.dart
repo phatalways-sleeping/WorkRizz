266d96539f13367b1e74ca3ca23be8ecdeb527f3
phat-always-sleeping <tkphat21@apcs.fitus.edu.vn>
2023-09-18 17:50:32 +0700

    fix(ui): fix

--- lib/widgets/tasklist/subtask_widget.dart
+++ lib/widgets/tasklist/subtask_widget.dart
@@ -14,163 +14,168 @@ class SubTaskWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      clipBehavior: Clip.hardEdge,
-      physics: const BouncingScrollPhysics(),
+    return Padding(
       padding: EdgeInsets.only(
-        bottom: context.mediaQuery.size.width * RATIO_PADDING,
+        bottom: context.mediaQuery.size.height * 0.08,
       ),
-      children: [
-        AppBar(
-          title: Row(
-            children: [
-              TaskTag(
-                color: GREY,
-                name: '$currentPage/$totalPage',
-              ),
-              SizedBox(width: context.mediaQuery.size.width * RATIO_PADDING),
-              Text(
-                task.name,
-                style: context.textTheme.titleMedium,
-              ),
-              const Spacer(),
-              if (context.watch<TasklistBloc>().state
-                  is TasklistSubscriptionEditable)
-                ElevatedButton(
-                  style: ButtonStyle(
-                    padding: MaterialStateProperty.all(
-                      EdgeInsets.zero,
-                    ),
-                    minimumSize: const MaterialStatePropertyAll(
-                      Size(
-                        60.0,
+      child: ListView(
+        shrinkWrap: true,
+        clipBehavior: Clip.hardEdge,
+        physics: const BouncingScrollPhysics(),
+        padding: EdgeInsets.only(
+          bottom: context.mediaQuery.size.width * RATIO_PADDING,
+        ),
+        children: [
+          AppBar(
+            title: Row(
+              children: [
+                TaskTag(
+                  color: GREY,
+                  name: '$currentPage/$totalPage',
+                ),
+                SizedBox(width: context.mediaQuery.size.width * RATIO_PADDING),
+                Text(
+                  task.name,
+                  style: context.textTheme.titleMedium,
+                ),
+                const Spacer(),
+                if (context.watch<TasklistBloc>().state
+                    is TasklistSubscriptionEditable)
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all(
+                        EdgeInsets.zero,
+                      ),
+                      minimumSize: const MaterialStatePropertyAll(
+                        Size(
+                          60.0,
+                          24.0,
+                        ),
+                      ),
+                      maximumSize: const MaterialStatePropertyAll(
+                        Size(
+                          60.0,
+                          24.0,
+                        ),
+                      ),
+                      iconSize: MaterialStateProperty.all(
                         24.0,
                       ),
-                    ),
-                    maximumSize: const MaterialStatePropertyAll(
-                      Size(
-                        60.0,
-                        24.0,
+                      backgroundColor: const MaterialStatePropertyAll(
+                        Colors.transparent,
                       ),
+                      shadowColor: const MaterialStatePropertyAll(
+                        Colors.transparent,
+                      ),
+                      elevation: const MaterialStatePropertyAll(
+                        0.0,
+                      ),
+                      alignment: Alignment.centerRight,
                     ),
-                    iconSize: MaterialStateProperty.all(
-                      24.0,
+                    child: const Icon(
+                      Icons.delete_outline_rounded,
+                      color: BLACK,
                     ),
-                    backgroundColor: const MaterialStatePropertyAll(
-                      Colors.transparent,
-                    ),
-                    shadowColor: const MaterialStatePropertyAll(
-                      Colors.transparent,
-                    ),
-                    elevation: const MaterialStatePropertyAll(
-                      0.0,
-                    ),
-                    alignment: Alignment.centerRight,
+                    onPressed: () async {
+                      await showDialog<bool>(
+                        context: context,
+                        builder: (context) => CustomDialog(
+                          title: "Remove this Task",
+                          leftText: "No",
+                          rightText: "Yes",
+                          leftColor: PURPLE,
+                          rightColor: PINK,
+                          focusleftColor: PALE,
+                          focusrightColor: GREEN,
+                          onLeftPressed: (context) {
+                            Navigator.of(context).pop(false);
+                          },
+                          onRightPressed: (context) {
+                            Navigator.of(context).pop(true);
+                          },
+                        ),
+                      ).then(
+                        (value) {
+                          if (value == null) {
+                            return;
+                          }
+                          if (value == true) {
+                            context.read<TasklistBloc>().add(
+                                  TasklistDeleteTask(
+                                    task.id,
+                                  ),
+                                );
+                          }
+                        },
+                      );
+                    },
                   ),
-                  child: const Icon(
-                    Icons.delete_outline_rounded,
+              ],
+            ),
+            titleSpacing: 0.0,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            toolbarHeight: context.mediaQuery.size.width * RATIO_PADDING * 2.5,
+          ),
+          ...List.generate(
+            task.subTaskSmallInformations.length,
+            (index) => SubTaskInfo(
+              subTaskSmallInformation: task.subTaskSmallInformations[index],
+              taskId: task.id,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () => context.read<NavigationBloc>().add(
+                  NavigateToSubTaskCreate(
+                    task.id,
+                  ),
+                ),
+            style: ButtonStyle(
+              alignment: Alignment.centerLeft,
+              shape: MaterialStatePropertyAll(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(MEDIUM_CORNER),
+                  side: const BorderSide(
                     color: BLACK,
+                    width: 1,
                   ),
-                  onPressed: () async {
-                    await showDialog<bool>(
-                      context: context,
-                      builder: (context) => CustomDialog(
-                        title: "Remove this Task",
-                        leftText: "No",
-                        rightText: "Yes",
-                        leftColor: PURPLE,
-                        rightColor: PINK,
-                        focusleftColor: PALE,
-                        focusrightColor: GREEN,
-                        onLeftPressed: (context) {
-                          Navigator.of(context).pop(false);
-                        },
-                        onRightPressed: (context) {
-                          Navigator.of(context).pop(true);
-                        },
-                      ),
-                    ).then(
-                      (value) {
-                        if (value == null) {
-                          return;
-                        }
-                        if (value == true) {
-                          context.read<TasklistBloc>().add(
-                                TasklistDeleteTask(
-                                  task.id,
-                                ),
-                              );
-                        }
-                      },
-                    );
-                  },
-                ),
-            ],
-          ),
-          titleSpacing: 0.0,
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent,
-          toolbarHeight: context.mediaQuery.size.width * RATIO_PADDING * 2.5,
-        ),
-        ...List.generate(
-          task.subTaskSmallInformations.length,
-          (index) => SubTaskInfo(
-            subTaskSmallInformation: task.subTaskSmallInformations[index],
-            taskId: task.id,
-          ),
-        ),
-        ElevatedButton(
-          onPressed: () => context.read<NavigationBloc>().add(
-                NavigateToSubTaskCreate(
-                  task.id,
                 ),
               ),
-          style: ButtonStyle(
-            alignment: Alignment.centerLeft,
-            shape: MaterialStatePropertyAll(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(MEDIUM_CORNER),
-                side: const BorderSide(
-                  color: BLACK,
-                  width: 1,
-                ),
+              backgroundColor: const MaterialStatePropertyAll(
+                BLACK,
               ),
-            ),
-            backgroundColor: const MaterialStatePropertyAll(
-              BLACK,
-            ),
-            elevation: MaterialStateProperty.resolveWith(
-              (states) => states.isPressed ? 4.0 : 0.0,
-            ),
-            overlayColor: const MaterialStatePropertyAll(
-              GREEN,
-            ),
-            splashFactory: InkSparkle.splashFactory,
-            animationDuration: const Duration(
-              seconds: 2,
-            ),
-            tapTargetSize: MaterialTapTargetSize.padded,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(width: context.mediaQuery.size.width * RATIO_PADDING),
-              SvgPicture.string(SvgAssets.addwhite),
-              SizedBox(
-                  width: context.mediaQuery.size.width *
-                      (RATIO_SPACE + RATIO_SPACE)),
-              Text(
-                'Add subtask',
-                style: context.textTheme.bodyMedium?.copyWith(
-                  color: WHITE,
-                ),
+              elevation: MaterialStateProperty.resolveWith(
+                (states) => states.isPressed ? 4.0 : 0.0,
               ),
-            ],
-          ),
-        )
-      ],
+              overlayColor: const MaterialStatePropertyAll(
+                GREEN,
+              ),
+              splashFactory: InkSparkle.splashFactory,
+              animationDuration: const Duration(
+                seconds: 2,
+              ),
+              tapTargetSize: MaterialTapTargetSize.padded,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(width: context.mediaQuery.size.width * RATIO_PADDING),
+                SvgPicture.string(SvgAssets.addwhite),
+                SizedBox(
+                    width: context.mediaQuery.size.width *
+                        (RATIO_SPACE + RATIO_SPACE)),
+                Text(
+                  'Add subtask',
+                  style: context.textTheme.bodyMedium?.copyWith(
+                    color: WHITE,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
